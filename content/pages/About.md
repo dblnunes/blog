53e042f3ebbdf5952250ff5d56a8ced30a39a615
Daniel Nunes <daninunes@daniels-mbp.mynet>
2018-04-02 12:43:52 +0100

    new theme

---
+++ content/pages/About.md
@@ -0,0 +1,12 @@
+Author: Daniel Nunes
+Title: About
+
+
+###About Me  
+
+I am an engineer and programmer by trade. I'll do just about anything on two wheels. I'm an occasional writer and a halfway decent sailor, too. I have backed my way into programming to a certain degree, but in 2012 I pushed myself into Python full-time and haven't looked back. I am curious about the world around me and find my natural bent is digging deep into data sets trying to find the underlying signals and patterns. Hence the blog name.  
+
+###About this blog  
+  
+I created this blog for a dual purpose - to give me a platform to share the worthwhile bits of data I come across, and to help others dive into similar projects themselves. It's tremendously fun to get your hands into a complex dataset and look for patterns. But too often the bulk of my time is spent just familiarizing myself with the environment or learning rookie mistakes the hard way. Sometimes though, a Google search turns up just the right blog post and suddenly, you can learn from someone else's mistakes and find the basic code to cover the steepest portion of the learning curve. I hope Beneath Data can show at least a couple people the way.
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Daniel Nunes'
-SITENAME = 'Daniel Nunes'
-SITESUBTITLE = 'Learning Data Science and Machine Learning'
-SITEURL = 'https://dblnunes.github.io/blog'
+AUTHOR = u'Daniel Nunes'
+SITENAME = u'Daniel Nunes'
+SITESUBTITLE = u'Learning Data Science and Machine Learning'
+SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
+
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
+DEFAULT_DATE = 'fs'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,16 +24,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = True
+
 # Blogroll
-LINKS = (('Publico', 'https://www.publico.pt/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'))
+LINKS = None
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/dblnunes'),
-          ('twitter', 'https://twitter.com/dbl_nunes'),('mailto', 'mailto:dblnunes@outlook.com'))
+          ('twitter', 'https://twitter.com/dbl_nunes'))
 
 DEFAULT_PAGINATION = 10
+
+HIDE_SIDEBAR = False
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_TAGS_INLINE = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -41,15 +53,18 @@ PLUGINS = ['ipynb.markup', 'i18n_subsites']
 OUTPUT_PATH = 'docs/'
 
 THEME = 'themes/pelican-themes/pelican-bootstrap3'
-BOOTSTRAP_THEME = 'cosmo'
+BOOTSTRAP_THEME = 'sandstone'
+BOOTSTRAP_NAVBAR_INVERSE = False
+BOOTSTRAP_FLUID = False
+
 SHOW_ARTICLE_CATEGORY = True
 
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
 STATIC_PATHS = ['images']
 
-SITELOGO = 'content/images/310logo.png'
-SITELOGO_SIZE = 32
+SITELOGO = None
+SITELOGO_SIZE = None
 
 
 ##
@@ -62,21 +77,20 @@ TAG_URL = 'tags/{slug}.html'
 TAG_SAVE_AS = 'tags/{slug}.html'
 TAGS_URL = 'tags.html'
 
+# Generate archive
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+
 ##
 
-ABOUT_ME = "I'm a programmer and engineer with a love for Python. I enjoy testing odd hypotheses, investigating datasets, and creating rad graphs.\
-<p>Find out more about me at <strong><a href=\"http://tylerhartley.com\" title=\"Professional Website\">tylerhartley.com</a></strong></p>\
-<p>You can also contact me " + """<a href="http://www.google.com/recaptcha/mailhide/d?k=01viQ7or9YI4gJ8hto_vDniA==&amp;c=PFSG4q4HL4celXjwzCtAo6YzW_WP9gWcjNfpI6f3Gxw=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501viQ7or9YI4gJ8hto_vDniA\75\75\46c\75PFSG4q4HL4celXjwzCtAo6YzW_WP9gWcjNfpI6f3Gxw\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address"><strong>here</strong></a></p>"""
-AVATAR = "/images/headshot.png"
 
-BANNER = "/images/banner.png"
+AVATAR = None
 
-DISPLAY_ARTICLE_INFO_ON_INDEX = True
-DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+BANNER = "images/kde3.png"
+BANNER_SUBTITLE = 'Learning Data Science and Machine Learning'
+
+DISPLAY_ARTICLE_INFO_ON_INDEX = False
+
 SHOW_ARTICLE_CATEGORY = True
 TAG_CLOUD_MAX_ITEMS = 8
 
-PYGMENTS_STYLE = 'monokai'
-
-# Generate archive
-YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+PYGMENTS_STYLE = 'native'
